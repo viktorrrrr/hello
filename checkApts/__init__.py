ee04d8cfb25cbf16128bea4cdbac235cd8a74963
Viktor <viktor.enlund@ef.cn>
2021-03-02 12:18:19 +0800

    1.0

---
+++ checkApts/__init__.py
@@ -0,0 +1,119 @@
+# Import module for logging purposes
+import logging
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import date
+import smtplib
+import lxml
+import openpyxl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+ 
+# Import module for Azure Functions and give it an alias
+import azure.functions as func
+
+def checkCompound(df, df_new, url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    apts = soup.find_all("div", {"class": "content__list--item"})
+
+    for ap in apts:
+        code = ap.attrs['data-house_code']
+        try:
+            codeList = df['code'].to_list()
+        except:
+            codeList = []
+
+        if code in codeList:
+            pass
+            #print(f"already exists {code}")
+        else:
+            print(f"new! {code}")
+            price = ap.find_all("span", {"class": "content__list--item-price"})
+            apt = {}
+            apt['code'] = code
+            apt['title'] = price[0].parent.contents[1].contents[1].contents[0]
+            apt['price'] = int(price[0].contents[0].contents[0])
+
+            i = ap.find_all("p", {"class": "content__list--item--des"})
+            sqm = i[0].contents[8].replace('\n','').replace('㎡','').replace(' ','')
+            sqm = int(sqm)
+            apt['sqm'] = sqm
+            apt['url'] = f'<a href=https://sh.zu.ke.com/zufang/{ap.attrs["data-house_code"]}.html>Link</a>'
+            apt['dateFound'] = date.today().strftime('%Y-%m-%d')
+            df = df.append(apt, ignore_index=True)
+
+            if sqm > 79:
+                if apt['price'] < 15000:
+                    df_new = df_new.append(apt, ignore_index=True)
+            else:
+                print(f'removing{code} - too small or pricey')
+
+
+    print("")
+    return df, df_new
+
+def sendEmail(df_new):
+
+    text = df_new.to_string()
+    html_msg = df_new.to_html(index_names=False, escape=False, index=False, columns=['price',	'sqm',	'title',	'url'])
+    msg = MIMEMultipart('alternative')
+
+    frm = 'weikeduo11@hotmail.com'
+    to = 'viktor.enlund@gmail.com'
+    msg['From'] = frm
+    msg['To'] = to
+    msg['Subject'] = 'New apartments available!'
+
+
+    hotmail_user = 'weikeduo11@hotmail.com'
+    hotmail_password = 'voctor766'
+    part1 = MIMEText(text, 'plain')
+    part2 = MIMEText(html_msg, 'html')
+    msg.attach(part1)
+    msg.attach(part2)
+
+    try:
+        server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(hotmail_user, hotmail_password)
+        server.sendmail(frm, to, msg.as_string())
+        server.close()
+    except:
+        print('Something went wrong...')
+
+
+# Main function and entry point of this Azure Function
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    # Log information
+    logging.info('Python HTTP trigger function processed a request.')
+ 
+    URL = {'https://sh.zu.ke.com/zufang/rco11rs%E4%B8%8A%E9%9D%92%E4%BD%B3%E5%9B%AD/', 'https://sh.zu.ke.com/zufang/brp7500erp14000rs%E6%B2%B3%E6%BB%A8%E5%9B%B4%E5%9F%8E/', 'https://sh.zu.ke.com/zufang/c5011000018183/?sug=%E9%9F%B3%E4%B9%90%E5%B9%BF%E5%9C%BA'}
+    try:
+        df = pd.read_excel('list.xlsx')
+    except:
+        df = pd.DataFrame()
+    
+    df_new = pd.DataFrame()
+
+    for u in URL:
+        df, df_new = checkCompound(df,df_new, u)
+
+    df.to_excel('list.xlsx', index=False)
+
+    if df_new.empty == False:
+        sendEmail(df_new)
+    else:
+        print("no new!")
+
+
+    return func.HttpResponse(status_code=200,headers={'content-type':'text/html'}, 
+        body=
+f"""<!DOCTYPE html>
+<html>
+<body>{df_new.to_html(index_names=False, escape=False, index=False)}
+</body>
+</html>
+""")
